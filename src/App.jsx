ad56783fb5718fa61d143ad30b7a5e525b680750
Devansh Sharma <devanshpaulistya@gmail.com>
2026-06-11 00:06:30 +0530

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+ 
+
+
+  return (
+    <>
+    <h1> Learn reaach with chai code .com</h1>
+    <p> this is a simple react app </p>
+    </>
+  )
+}
+
+export default App
